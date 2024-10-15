65bd9de980e0e0fd2b23f9ef6718cea6b3af03ba
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-10-15 22:07:55 +0530

    Fix:Fixed environment variable issue

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,13 +4,10 @@ dotenv.config();
 
 
 module.exports = {
-  PORT: process.env.PORT || 5000,
-  REDIS_PORT: process.env.REDIS_PORT || "6379",
-  REDIS_HOST: process.env.REDIS_HOST || "127.0.0.1",
-  ATLAS_DB_URL:
-    process.env.ATLAS_DB_URL ||
-    "mongodb+srv://prakharagarwal70:MVMKAkFTCAQ4HZet@cluster0.d4ovd.mongodb.net/",
-  NODE_ENV: process.env.NODE_ENV || "development",
-  PROBLEM_ADMIN_SERVICE_URL:
-    process.env.PROBLEM_ADMIN_SERVICE_URL || "http:localhost:3000",
+  PORT: process.env.PORT,
+  REDIS_PORT: process.env.REDIS_PORT,
+  REDIS_HOST: process.env.REDIS_HOST,
+  ATLAS_DB_URL:process.env.ATLAS_DB_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  PROBLEM_ADMIN_SERVICE_URL:process.env.PROBLEM_ADMIN_SERVICE_URL,
 };
